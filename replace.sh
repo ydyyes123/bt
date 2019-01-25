1ee7a5d86cbc247d221d64c476c7951cded7ba0c
ydyyes <ydyyes@163.com>
2019-01-25 20:52:29 +0800

    增加重启bt

--- replace.sh
+++ replace.sh
@@ -2,4 +2,5 @@
 cp /www/server/panel/task.py /www/server/panel/task.py.bk
 \cp -fr ./www/server/panel/task.py /www/server/panel/
 \cp -fr ./www/server/panel/mail.json /www/server/panel/
+/etc/init.d/bt restart
 echo "执行结束!"
